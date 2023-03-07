b93f593a24a9bb7fa89ebba7860d5cd4f791203b
juhongyee <88369452+juhongyee@users.noreply.github.com>
2023-03-07 23:05:01 +0900

    Add files via upload

--- Env2048.py
+++ Env2048.py
@@ -1,20 +1,22 @@
-import numpy as np
 import random
+import copy
 
 BOARD_SIZE = 4
 
 class Game2048:
     def __init__(self):
-        self.grid = np.zeros((BOARD_SIZE,BOARD_SIZE))
+        self.grid = [[0 for _ in range(BOARD_SIZE)] for _ in range(BOARD_SIZE)]
         self.score = 0
         self.add_new_tile()
         self.add_new_tile()
 
     def reset(self):
-        self.grid = np.zeros((BOARD_SIZE,BOARD_SIZE))
+        self.grid = [[0 for _ in range(BOARD_SIZE)] for _ in range(BOARD_SIZE)]
         self.score = 0
         self.add_new_tile()
         self.add_new_tile()
+        
+        return self.grid
     
     def add_new_tile(self):
         empty_cells = [(i, j) for i in range(BOARD_SIZE) for j in range(BOARD_SIZE) if self.grid[i][j] == 0]
@@ -30,12 +32,12 @@ class Game2048:
         return True
     
     #for dictionary
-    def grid_to_tuple(grid):
-            for i in range(BOARD_SIZE):
-                grid[i] = tuple(grid[i])
-            grid = tuple(grid)
-            
-            return grid
+    def grid_to_tuple(self,board):
+        for i in range(BOARD_SIZE):
+            board[i] = tuple(board[i])
+        board = tuple(board)
+        
+        return board
         
     def move_left(self,simulation):
         if not (simulation==1):
@@ -55,7 +57,7 @@ class Game2048:
                         self.grid[i][j] = 0
                         self.score += self.grid[i][j-1]
         else:
-            grid = self.grid
+            grid = copy.deepcopy(self.grid)
             score = self.score
             for i in range(BOARD_SIZE):
                 # move tiles to the left
@@ -93,7 +95,7 @@ class Game2048:
                         self.grid[i][j] = 0
                         self.score += self.grid[i][j+1]
         else:
-            grid = self.grid
+            grid = copy.deepcopy(self.grid)
             score = self.score
             for i in range(BOARD_SIZE):
                 # move tiles to the right
@@ -129,7 +131,7 @@ class Game2048:
                         self.grid[i][j] = 0
                         self.score += self.grid[i-1][j]
         else:
-            grid = self.grid
+            grid = copy.deepcopy(self.grid)
             score = self.score
             for j in range(BOARD_SIZE):
                 # move tiles up
@@ -145,6 +147,7 @@ class Game2048:
                         grid[i-1][j] *= 2
                         grid[i][j] = 0
                         score += grid[i-1][j]
+                        
             grid = self.grid_to_tuple(grid)
             return grid
 
@@ -158,35 +161,32 @@ class Game2048:
                         while k < BOARD_SIZE-1 and self.grid[k+1][j] == 0:
                             self.grid[k+1][j], self.grid[k][j] = self.grid[k][j], self.grid[k+1][j]
                             k += 1
+                            
                 # merge adjacent tiles
-                for i in range(BOARD_SIZE-1, -1, -1):
+                for i in range(BOARD_SIZE-2, -1, -1):
                     if self.grid[i][j] == self.grid[i+1][j] and self.grid[i][j] != 0:
                         self.grid[i+1][j] *= 2
                         self.grid[i][j] = 0
                         self.score += self.grid[i+1][j]
         else:
-            grid = self.grid
+            grid = copy.deepcopy(self.grid)
             score = self.score
             for j in range(BOARD_SIZE):
                 # move tiles down
-                for i in range(BOARD_SIZE-2, -1, -1):
+                for i in range(2, -1, -1):
                     if grid[i][j] != 0:
                         k = i
                         while k < BOARD_SIZE-1 and grid[k+1][j] == 0:
                             grid[k+1][j], grid[k][j] = grid[k][j], grid[k+1][j]
                             k += 1
                 # merge adjacent tiles
-                for i in range(BOARD_SIZE-1, -1, -1):
+                for i in range(2, -1, -1):
                     if grid[i][j] == grid[i+1][j] and grid[i][j] != 0:
                         grid[i+1][j] *= 2
                         grid[i][j] = 0
                         score += grid[i+1][j]
             grid = self.grid_to_tuple(grid)
             return grid
-        
-            
-    def return_reward(self):
-        return self.score
     
     def print_grid(self):
         print('+------' * BOARD_SIZE + '+')
@@ -199,3 +199,27 @@ class Game2048:
                     print('{:^6}'.format(str(self.grid[i][j])), end='|')
             print()
             print('+------' * BOARD_SIZE + '+')
+    
+    def step(self,action):
+        if action == 'L':
+            self.move_left(0)
+        
+        elif action == 'R':
+            self.move_right(0)
+        
+        elif action == 'U':
+            self.move_up(0)
+        
+        elif action == 'D':
+            self.move_down(0)
+        
+        Flag = self.add_new_tile()
+        
+        #Terminal
+        if not Flag:
+            return (False,self.grid,self.score)
+        
+        else:
+            return (True,self.grid,self.score)
+        
+        
